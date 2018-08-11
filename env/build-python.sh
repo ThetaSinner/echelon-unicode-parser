ef3a0a0051c36f3c8201ce35443eff4e352acdca
ThetaSinner <ThetaSinner@users.noreply.github.com>
2018-08-11 14:37:58 +0100

    check in scripts for setting up python for benchmarking

---
+++ env/build-python.sh
@@ -0,0 +1,14 @@
+
+apt update
+apt upgrade
+
+# TODO Some of these libraries have newer versions, check for build compatibility next time this script is run.
+apt install -y build-essential libssl-dev zlib1g-dev libncurses5-dev libncursesw5-dev libreadline-dev libsqlite3-dev libgdbm-dev libdb5.3-dev libbz2-dev libexpat1-dev liblzma-dev tk-dev libffi-dev
+
+mkdir -p /opt/python && cd /opt/python
+wget https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tar.xz
+tar xf Python-3.7.0.tar.xz
+cd Python-3.7.0
+./configure --enable-optimizations
+make -j 8
+make altinstall
---
+++ env/setup-scipy.sh
@@ -0,0 +1,2 @@
+python3.7 -m pip install --upgrade pip
+python3.7 -m pip install numpy scipy matplotlib ipython jupyter pandas sympy nose
